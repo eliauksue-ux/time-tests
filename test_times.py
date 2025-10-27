154a7e4dd35fd5c460c955f77a5dd07345e49aaa
Miao Su <miao.su.25@ucl.ac.uk>
2025-10-27 10:34:32 +0000

    three more test functions are added

--- test_times.py
+++ test_times.py
@@ -2,9 +2,38 @@ from times import compute_overlap_time, time_range
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    
+
     result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), 
                 ('2010-01-12 10:38:00', '2010-01-12 10:45:00')] 
     
+    assert result == expected
+
+def test_no_overlap():
+    range1 = time_range("2022-01-01 10:00:00", "2022-01-01 11:00:00")
+    range2 = time_range("2022-01-01 12:00:00", "2022-01-01 13:00:00")
+
+    result = compute_overlap_time(range1, range2)
+    expected = []
+
+    assert result == expected
+
+def test_multiple_intervals():
+    range1 = time_range("2022-01-01 10:00:00", "2022-01-01 12:00:00", 3)
+    range2 = time_range("2022-01-01 11:00:00", "2022-01-01 13:00:00", 3)
+
+    result = compute_overlap_time(range1, range2)
+    expected = [
+    ('2022-01-01 11:00:00', '2022-01-01 11:20:00'),
+    ('2022-01-01 11:20:00', '2022-01-01 11:40:00'),
+    ('2022-01-01 11:40:00', '2022-01-01 12:00:00')
+    ]
+
+    assert result == expected
+
+def test_touching_intervals():
+    range1 = [("2010-01-12 10:00:00", "2010-01-12 11:00:00")]
+    range2 = [("2010-01-12 11:00:00", "2010-01-12 12:00:00")]
+    result = compute_overlap_time(range1, range2)
+    expected = [("2010-01-12 11:00:00","2010-01-12 11:00:00")]
     assert result == expected
